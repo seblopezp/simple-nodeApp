857020630d187130f2dfc3e9331be4a88f5d796d
Sebastián López <seblopezp@gmail.com>
2023-01-11 23:30:15 -0300

    ADD: sensor service

---
+++ src/services/SensorService.js
@@ -0,0 +1,17 @@
+const SensorModel = require("../models/Sensor");
+ 
+exports.getAllData = async () => {
+  return await SensorModel.find();
+};
+ 
+exports.createData = async (blog) => {
+  return await SensorModel.create(blog);
+};
+
+exports.updateData = async (id, blog) => {
+  return await SensorModel.findByIdAndUpdate(id, blog);
+};
+ 
+exports.deleteData = async (id) => {
+  return await SensorModel.findByIdAndDelete(id);
+};
